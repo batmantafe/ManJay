9a5ced6ace6e52fc6e61b1819abc5ad794923636
Duane Reymond <duane.reymond@gmail.com>
2017-11-30 22:29:47 +1100

    -CustomiseSet script added. Colour saving and loading between Customise/Game scenes for Head and Body.

---
+++ Assets/Game/Scripts/CustomiseSet.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class CustomiseSet : MonoBehaviour
+{
+    public Material[] mats;
+
+    public GameObject head, body;
+
+    public int headMatsIndex, bodyMatsIndex, matsMaxIndex;
+
+    // Use this for initialization
+    void Start()
+    {
+        //headMatsIndex = 0; // Set headMatsIndex to Zero as Default
+        //bodyMatsIndex = 0;
+
+        Load();
+
+        matsMaxIndex = mats.Length - 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void HeadColourButton()
+    {
+        if (headMatsIndex < matsMaxIndex)
+        {
+            head.GetComponent<Renderer>().material = mats[headMatsIndex + 1]; // whatever Mats is, plus One
+            headMatsIndex = headMatsIndex + 1;
+            return;
+        }
+
+        if (headMatsIndex == matsMaxIndex)
+        {
+            head.GetComponent<Renderer>().material = mats[0]; // reset Mats to Zero
+            headMatsIndex = 0;
+            return;
+        }
+    }
+
+    public void BodyColourButton()
+    {
+        if (bodyMatsIndex < matsMaxIndex)
+        {
+            body.GetComponent<Renderer>().material = mats[bodyMatsIndex + 1]; // whatever Mats is, plus One
+            bodyMatsIndex = bodyMatsIndex + 1;
+            return;
+        }
+
+        if (bodyMatsIndex == matsMaxIndex)
+        {
+            body.GetComponent<Renderer>().material = mats[0]; // reset Mats to Zero
+            bodyMatsIndex = 0;
+            return;
+        }
+    }
+
+    public void SaveAndPlayButton()
+    {
+        PlayerPrefs.SetInt("Head Colour", headMatsIndex);
+        PlayerPrefs.SetInt("Body Colour", bodyMatsIndex);
+
+        Debug.Log("headMatsIndex = " + headMatsIndex);
+        Debug.Log("bodyMatsIndex = " + bodyMatsIndex);
+
+        SceneManager.LoadScene("Game");
+    }
+
+    void Load()
+    {
+        headMatsIndex = PlayerPrefs.GetInt("Head Colour");
+        bodyMatsIndex = PlayerPrefs.GetInt("Body Colour");
+
+        head.GetComponent<Renderer>().material = mats[headMatsIndex];
+        body.GetComponent<Renderer>().material = mats[bodyMatsIndex];
+    }
+}
